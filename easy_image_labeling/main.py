335391c2ed6ba9c72befc03c05e6182ad06b3ec6
NiklasMueller2001 <n.mueller@medienagentur24.eu>
2025-02-05 16:19:24 +0100

    add csrf protection

--- easy_image_labeling/main.py
+++ easy_image_labeling/main.py
@@ -1,11 +1,13 @@
-print(__name__)
 from easy_image_labeling import create_app
-import pathlib
 from flask import render_template
 from waitress import serve
 from werkzeug.utils import secure_filename
+from flask_wtf import CSRFProtect
 
 app = create_app()
+csrf = CSRFProtect(app)
+print()
+
 
 @app.route("/")
 @app.route("/index")
@@ -15,4 +17,4 @@ def index():
 
 if __name__ == "__main__":
     # serve(app, host="0.0.0.0", port=8080)
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)  
