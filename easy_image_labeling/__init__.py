6c777fc337f53f2c0f237941612b099d378b2e0f
NiklasMueller2001 <n.mueller@medienagentur24.eu>
2025-02-05 20:48:44 +0100

    app secret
    - Added app secret in create_app function.

--- easy_image_labeling/__init__.py
+++ easy_image_labeling/__init__.py
@@ -4,8 +4,10 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__, template_folder="./templates")
-    from easy_image_labeling.pages import config
-    app.register_blueprint(config.bp)
+    with open("secret.env") as f:
+        app.secret_key = f.read()
+    from easy_image_labeling.pages import selection
+    app.register_blueprint(selection.bp)
     try:
         os.makedirs(app.instance_path)
     except OSError:
