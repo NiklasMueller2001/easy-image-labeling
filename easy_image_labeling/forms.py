3757ea7b1cc8a80846da3bf18b6e1d8c1a7fa461
NiklasMueller2001 <n.mueller@medienagentur24.eu>
2025-02-09 13:51:02 +0100

    upload dataset
    - Added a form with custom validator for the
    upload of entire folders.
    
    - Added function to handle file upload.
    
    - Added template for file upload.
    
    - Minor change class_number template.

--- easy_image_labeling/forms.py
+++ easy_image_labeling/forms.py
@@ -1,9 +1,11 @@
 from typing import Any
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileRequired
 from wtforms import (
     FieldList,
     FormField,
     Form,
+    MultipleFileField,
     ValidationError,
     StringField,
     SubmitField,
@@ -11,7 +13,7 @@ from wtforms import (
 from wtforms.validators import InputRequired, Length
 
 
-class DuplicateValidator:
+class DuplicateStringInputValidator:
     """
     Custom validator to ensure no duplicate values were entered for
     forms that contain field lists.
@@ -36,9 +38,82 @@ class DuplicateValidator:
             raise ValidationError("Fields must not contain duplicate data.")
 
 
+class NoSpecialCharactersValidator:
+    """
+    Custom validator to ensure the entered text in the input field does
+    not contain any of the specified characters.
+    """
+
+    def __init__(self, excluded_characters: list[str] | None = None) -> None:
+        """
+        Parameters
+        ----------
+        field_name: str
+            Set name of fields inside field list.
+        """
+        if excluded_characters is None:
+            self.excluded_characters = [
+                " ",
+                "/",
+                "\\",
+                "*",
+                "~",
+                "+",
+                "&",
+                "%",
+                "$",
+                "§",
+            ]
+        else:
+            self.excluded_characters = excluded_characters
+
+    def __call__(self, form: Form, field: StringField) -> Any:
+        input = field.data
+        if input is None:
+            raise ValidationError("Invalid input.")
+        for excluded_character in self.excluded_characters:
+            if " " in input:
+                raise ValidationError(
+                    f"Input must not contain any '{excluded_character}' characters"
+                )
+
+
+class PictureFolderValidator:
+    """
+    Checks if selected folder in upload form contains at least one file
+    and checks if all files inside folder have supported extensions.
+    """
+
+    def __init__(self, allowed_extensions: tuple[str] | None = None) -> None:
+        if allowed_extensions is None:
+            self.allowed_extensions = ("jpg", "png", "pdf")
+        else:
+            self.allowed_extensions = allowed_extensions
+
+    def __call__(self, form: Form, field: MultipleFileField) -> Any:
+        if not form.files.data:  # type: ignore
+            raise ValidationError("No files were selected.")
+
+        for file in form.files.data:  # type: ignore
+            if not file.filename:  # Ensures the file has a valid name
+                raise ValidationError("Empty file detected.")
+            if not (
+                "." in file.filename
+                and file.filename.rsplit(".", 1)[1] in self.allowed_extensions
+            ):
+                raise ValidationError(
+                    f"Invalid file type: {file.filename}.\nAllowed filetypes are {self.allowed_extensions}"
+                )
+
+
 class LabelNameForm(FlaskForm):
     label_name = StringField(
-        "Label", validators=[InputRequired(), Length(min=1, max=25)]
+        "Label",
+        validators=[
+            InputRequired(),
+            Length(min=1, max=20),
+            NoSpecialCharactersValidator(),
+        ],
     )
 
 
@@ -46,7 +121,28 @@ class LabelNameFormContainer(FlaskForm):
     label_names = FieldList(
         FormField(LabelNameForm),
         "All Labels",
-        validators=[DuplicateValidator("label_name")],
+        validators=[DuplicateStringInputValidator("label_name")],
         min_entries=0,
     )
     submit = SubmitField()
+
+
+class UploadFolderForm(FlaskForm):
+    files = MultipleFileField(
+        "Upload Files",
+        validators=[
+            PictureFolderValidator(),
+            FileAllowed(
+                ["pdf", "jpg", "png"], "Only .png, .pdf or .jpg file types allowed!"
+            ),
+        ],
+    )
+    dataset_name = StringField(
+        "Dataset name",
+        validators=[
+            InputRequired(),
+            Length(min=1, max=20),
+            NoSpecialCharactersValidator(),
+        ],
+    )
+    submit = SubmitField("Upload Files")
--- easy_image_labeling/pages/selection.py
+++ easy_image_labeling/pages/selection.py
@@ -1,14 +1,17 @@
+from easy_image_labeling.forms import LabelNameFormContainer, UploadFolderForm
 from flask import (
     Blueprint,
+    current_app,
+    flash,
     g,
-    render_template,
-    redirect,
     url_for,
+    redirect,
+    render_template,
     request,
     session,
-    flash,
 )
-from easy_image_labeling.forms import LabelNameFormContainer
+from pathlib import Path
+from werkzeug.utils import secure_filename
 
 bp = Blueprint("config", __name__)
 
@@ -30,6 +33,7 @@ def set_number_of_classes():
 
 @bp.route("/upload_dataset", methods=["POST", "GET"])
 def set_class_names():
+    upload_form = UploadFolderForm()
     if request.method == "POST":
         label_names_form = LabelNameFormContainer()
         label_names_form.process(request.form)
@@ -39,4 +43,33 @@ def set_class_names():
         for i, label_name in enumerate(label_names_form.label_names.data):
             label_names[f"label_{i}"] = label_name["label_name"]
         session["label_names"] = label_names
-    return "<br>".join(session["label_names"])
+    return render_template("upload_dataset.html", form=upload_form)
+
+
+@bp.route("/", methods=["POST"])
+def upload_folder():
+    if request.method == "POST":
+        upload_form = UploadFolderForm()
+        upload_form.process(request.form)
+        uploaded_files = request.files.getlist("files")
+        dataset_name = upload_form.dataset_name.data
+        if dataset_name is None:
+            flash("Invalid dataset name.")
+            return render_template("index.html")
+        dataset_name = secure_filename(dataset_name)
+        upload_form.files.data = uploaded_files
+        if upload_form.validate_on_submit():
+            upload_path = current_app.config["DATASET_FOLDER"] / dataset_name
+            if not Path(upload_path).exists():
+                Path(upload_path).mkdir()  # Ensure upload directory exists
+            for file in upload_form.files.data:
+                filename = secure_filename(file.filename)
+                file.save(upload_path / filename)
+
+            flash("Files uploaded successfully!", "success")
+            return render_template("index.html")
+        for field in upload_form.errors:
+            if upload_form.errors[field]:
+                for error in upload_form.errors[field]:
+                    flash(error)
+    return redirect(url_for("config.set_class_names"))
