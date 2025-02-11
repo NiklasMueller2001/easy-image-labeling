17bd1109f14c6c54357f30d4808bdbfd2718d0cc
NiklasMueller2001 <n.mueller@medienagentur24.eu>
2025-02-11 10:26:54 +0100

    change dataset path
    - Changed dataset path to be located inside
    the static directory.
    
    - Adjusted .gitignore accordingly.

--- easy_image_labeling/config.py
+++ easy_image_labeling/config.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 
+
 class Config:
     with open("secret.env") as f:
         SECRET_KEY = f.read()
     MAX_FILE_SIZE = 5 * 1024 * 1024  # 10MB
-    DATASET_FOLDER = Path(__file__).parent / "datasets"
+    DATASET_FOLDER = Path(__file__).parent / "static" / "datasets"
+
 
 class DevConfig(Config):
     DEBUG = True
+
 
 class ProdConfig(Config):
     DEBUG = False
