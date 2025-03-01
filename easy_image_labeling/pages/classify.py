2a142471cd9cd9dfbd51a86bb12dd081b768ace9
NiklasMueller2001 <n.mueller@medienagentur24.eu>
2025-03-01 17:50:04 +0100

    classify next
    - Added function to direct user to a page where
    images of a specified dataset with a specified
    dataset id can be labelled by the user.
    
    - Added convenience function to retrieve the
    lowest dataset id of all unlabelled images inside
    a specified dataset redirect to the classification
    page of that image.
    
    - Added href in navbar to classify next image for
    chosen dataset.
    
    - Adjusted size of images in style.css.

--- easy_image_labeling/pages/classify.py
+++ easy_image_labeling/pages/classify.py
@@ -1,8 +1,43 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, current_app
+from easy_image_labeling.db.db import (
+    sqlite_connection,
+    get_lowest_image_id,
+    get_labels,
+    get_image_name,
+)
+from easy_image_labeling.dataset_manager import DatasetManager
+from easy_image_labeling.forms import MutliButtonForm
+from pathlib import Path
 
 bp = Blueprint("classify", __name__)
 
 
+def create_multibutton_form(labels: list[str]) -> MutliButtonForm:
+    multi_button_form = MutliButtonForm()
+    for i, label in enumerate(labels):
+        multi_button_form.label_buttons.append_entry()
+        multi_button_form.label_buttons[i].label.text = label
+    return multi_button_form
+
+
 @bp.route("/classify/<dataset>", methods=["POST", "GET"])
-def classify(dataset: str):
-    return dataset
+def classify_next_image(dataset: str):
+    with sqlite_connection(current_app.config["DB_URL"]) as cur:
+        image_id = get_lowest_image_id(cur, dataset)
+    return redirect(url_for("classify.classify", dataset=dataset, id=image_id))
+
+
+@bp.route("/classify/<dataset>/<id>", methods=["POST", "GET"])
+def classify(dataset: str, id: int):
+    """
+    Render html template for displaying one image from given Dataset
+    and DatasetID and form for labelling said image.
+    """
+    with sqlite_connection(current_app.config["DB_URL"]) as cur:
+        dataset_labels = get_labels(cur, dataset)
+        image_name = get_image_name(cur, dataset, id)
+    image_address = f"datasets/{dataset}/{image_name}"
+    multi_button_form = create_multibutton_form(dataset_labels)
+    return render_template(
+        "classify_image.html", image=image_address, form=multi_button_form
+    )
