3265e446f055a4e15951aced50a24aaca4d4335a
NiklasMueller2001 <n.mueller@medienagentur24.eu>
2025-05-02 18:32:53 +0200

    better exception in config.py
    - Added new custom exception MissingSecretEnvFile,
    which is raised when no 'secret.env' file is found
    when trying to load the Config class.
    
    - Added error message in config.py that informs
    users to create a secret.env file.

--- easy_image_labeling/config.py
+++ easy_image_labeling/config.py
@@ -1,9 +1,20 @@
 from pathlib import Path
+from easy_image_labeling.exceptions import MissingSecretEnvFile
 
 
 class Config:
-    with open("secret.env") as f:
-        SECRET_KEY = f.read()
+    try:
+        with open("secret.env") as f:
+            SECRET_KEY = f.read()
+    except FileNotFoundError:
+        raise MissingSecretEnvFile(
+            "No 'secret.env' file found. This file is used to store"
+            " your flask CSRF secret key. If this is your fist time"
+            " using easy_image_labeling you need to manually create"
+            " this file and insert your secret key. To learn more"
+            " about CSRF protection in flask you can visit"
+            " https://flask-wtf.readthedocs.io/en/0.15.x/csrf/."
+        )
     MAX_FILE_SIZE = 5 * 1024 * 1024  # 10MB
     DATASET_FOLDER = Path(__file__).parent / "static" / "datasets"
     DB_URL = Path(__file__).parent / "db" / "database.sqlite"
